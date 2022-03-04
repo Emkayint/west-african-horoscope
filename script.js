552c07a5c4456ee014addee5ed78bb3565fd41fe
Martin <mkmartinoes@gmail.com>
2022-03-04 07:30:10 +0300

    ft: converting input to string

---
+++ script.js
@@ -0,0 +1,11 @@
+let day = 12;
+let month = 25;
+let year = 1767;
+
+
+function validate(){
+    let myday = day.toString();
+    let mymonth = day.toString();
+    let myyear = year.toString();
+
+}
