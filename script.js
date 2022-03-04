c5fe80be6f68ab62ed786fd5a45071ddb948f701
Martin <mkmartinoes@gmail.com>
2022-03-04 16:27:22 +0300

    ft: verify year entered is correct

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let day = 30;
-let month = 12;
-let year = 1767;
+let month = 1;
+let year = 17672;
 
 let monthsDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
@@ -11,8 +11,16 @@ function validate(){
     let myyear = year.toString();
 
     if(day > monthsDays[month-1] || day < 1 || myday.length > 2){
-        console.log("error")
+        console.log("error");
+    } else if (month > 12 || month < 1){
+        console.log("error");
+    } else if (year < 1000 || myyear.length > 4){
+        console.log("error wrong ");
     }
     
 }
 validate()
+
+// function calculate(){
+
+// }
