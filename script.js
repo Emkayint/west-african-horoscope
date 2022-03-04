4efa7ffeb47aba393882b57d73e5a9cff895fd60
Martin <mkmartinoes@gmail.com>
2022-03-04 16:10:50 +0300

    fix: array - months array

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-let day = -1;
-let month = 1;
+let day = 1;
+let month = 12;
 let year = 1767;
 
-let monthsDays = [
-    [1, 31],
-    [2, 28],
-    [3, 31],
-    [4, 30],
-    [5, 31],
-    [6, 30],
-    [7, 31],
-    [8, 31],
-    [9, 30],
-    [10, 31],
-    [11, 30],
-    [12, 31]
-]
+let monthsDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
-console.log(monthsDays[month-1][1])
+console.log(monthsDays[month-1])
 function validate(){
     let myday = day.toString();
     let mymonth = day.toString();
     let myyear = year.toString();
 
-    if(day > monthsDays[month-1][1] || day < 1){
+    if(day > monthsDays[month-1] || day < 1){
         console.log("error")
     }
     
