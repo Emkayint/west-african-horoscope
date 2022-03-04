32c191ae35e03a41b4eceb7ced63fa8d6ce4571b
Martin <mkmartinoes@gmail.com>
2022-03-04 07:49:28 +0300

    bug: assigning days of month to month

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let day = 12;
-let month = 25;
+let month = 2;
 let year = 1767;
 
+let monthsDays = [
+    [1, 31],
+    [2, 28],
+    [3, 31],
+    [4, 30],
+    [5, 31],
+    [6, 30],
+    [7, 31],
+    [8, 31],
+    [9, 30],
+    [10, 31],
+    [11, 30],
+    [12, 31]
+]
 
+console.log(monthsDays[month-1][1])
 function validate(){
     let myday = day.toString();
     let mymonth = day.toString();
     let myyear = year.toString();
 
+    
 }
